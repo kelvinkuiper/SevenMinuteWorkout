6dc02543a5d2b42f9c413c2082496afa45975bd8
Kelvin Kuiper <kelvin@bekent.nl>
2013-05-29 10:57:08 +0200

    Replaced Runnable with CountDownTimer

--- SevenMinuteWorkout/src/main/java/nl/kelvinteractive/sevenminuteworkout/WorkoutActivity.java
+++ SevenMinuteWorkout/src/main/java/nl/kelvinteractive/sevenminuteworkout/WorkoutActivity.java
@@ -3,8 +3,7 @@ package nl.kelvinteractive.sevenminuteworkout;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
+import android.os.CountDownTimer;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,12 +13,11 @@ public class WorkoutActivity extends Activity {
     TextView workoutLabel;
     ImageView workoutImage;
 
+    int totalWorkouts;
     int cWorkoutIndex;
     int cTimer = 5;
 
     String cWorkout = null;
-
-    private Handler handler = new Handler();
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,60 +28,46 @@ public class WorkoutActivity extends Activity {
         workoutImage = (ImageView) findViewById(R.id.workoutImage);
 
         Intent intent = getIntent();
+
         /* get data from intent (workout_id) */
         cWorkoutIndex = intent.getIntExtra("workout_id", 0);
         /* set workout as current */
         cWorkout = ((MyApp) this.getApplication()).getWorkout(cWorkoutIndex);
-
         /* set workout title */
         workoutLabel.setText(cWorkout);
-
         /* set workout image */
         int resID = getResources().getIdentifier("w" + (cWorkoutIndex + 1), "drawable",  getPackageName());
         workoutImage.setImageResource(resID);
 
-        /* start the timer */
-        handler.postDelayed(updateTimerTask, 1000);
-    }
+        totalWorkouts = ((MyApp) WorkoutActivity.this.getApplication()).getWorkouts().length;
 
-    private Runnable updateTimerTask;
-    {
-        updateTimerTask = new Runnable() {
+        new CountDownTimer(cTimer * 1000, 1000) {
 
-            public void run() {
-
-                cTimer--;
-                timeLabel.setText(Integer.toString(cTimer));
-
-                if (cTimer > 0) {
-                    handler.postDelayed(this, 1000);
-                } else {
-                    /* start new activity with the current workout */
-                    int nextWorkout = cWorkoutIndex + 1;
-                    int totalWorkouts = ((MyApp) WorkoutActivity.this.getApplication()).getWorkouts().length;
-
-                    Log.i("NextWorkout", Integer.toString(nextWorkout));
-                    Log.i("TotalWorkouts", Integer.toString(totalWorkouts));
-
-                    if(nextWorkout == totalWorkouts){
-                        Intent intent = new Intent(WorkoutActivity.this, EndActivity.class);
-                        WorkoutActivity.this.startActivity(intent);
-                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                        closeWorkout();
-                    }
-                    else {
-                        Intent intent = new Intent(WorkoutActivity.this, PreWorkoutActivity.class);
-                        intent.putExtra("workout_id", nextWorkout);
-                        WorkoutActivity.this.startActivity(intent);
-                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-                        closeWorkout();
-                    }
-
-
-                }
-
+            public void onTick(long millisUntilFinished) {
+                timeLabel.setText(Long.toString(millisUntilFinished / 1000));
             }
-        };
+
+            public void onFinish() {
+
+                int nextWorkout = cWorkoutIndex + 1;
+
+                /* start new activity with the current workout */
+                if(nextWorkout == totalWorkouts){
+                    Intent intent = new Intent(WorkoutActivity.this, EndActivity.class);
+                    WorkoutActivity.this.startActivity(intent);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    closeWorkout();
+                }
+                else {
+                    Intent intent = new Intent(WorkoutActivity.this, PreWorkoutActivity.class);
+                    intent.putExtra("workout_id", nextWorkout);
+                    WorkoutActivity.this.startActivity(intent);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    closeWorkout();
+                }
+            }
+        }.start();
+
     }
 
     @Override
@@ -93,7 +77,6 @@ public class WorkoutActivity extends Activity {
     }
 
     public void closeWorkout() {
-        handler.removeCallbacks(updateTimerTask);
         finish();
     }
 }
